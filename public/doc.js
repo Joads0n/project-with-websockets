9ba096e4e5b26b68c3230e531dabd598e9c70cca
Joadson Silva <joadson83@hotmail.com>
2023-01-11 16:59:45 -0300

    Do servidor para os clientes

--- public/doc.js
+++ public/doc.js
@@ -1,1 +1,11 @@
 const socket = io();
+
+const textEditor = document.getElementById("editor-texto");
+
+textEditor.addEventListener("keyup", () => {
+    socket.emit("texto_editor", textEditor.value);
+});
+
+socket.on("texto_editor_clientes", (text) => {
+    textEditor.value = text;
+})
--- src/socket-back.js
+++ src/socket-back.js
@@ -1,5 +1,10 @@
 import io from "./server.js";
 
-io.on("connection", socket => {
+io.on("connection", (socket) => {
     console.log('um cliente se conectou! ID:', socket.id);
+
+    socket.on("texto_editor", (texto) => {
+        socket.broadcast.emit("texto_editor_clientes", texto);
+    });
 });
+
