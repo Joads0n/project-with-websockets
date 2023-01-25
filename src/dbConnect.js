883244cf857e0cf9750dff5355f275421e5e95af
Joadson Silva <joadson83@hotmail.com>
2023-01-25 16:02:01 -0300

    Conecção com  mongodb

---
+++ src/dbConnect.js
@@ -0,0 +1,20 @@
+import * as dotenv from 'dotenv';
+import { MongoClient } from "mongodb";
+
+dotenv.config();
+
+const client = new MongoClient(`mongodb+srv://joadson:${process.env.PASSWORLD}@alura.er1r1.mongodb.net/?retryWrites=true&w=majority`);
+
+let documentosColecao;
+
+try {
+    await client.connect();
+    const db = client.db('alura-websockets');
+    documentosColecao = db.collection("documentos");
+    
+    console.log("Conectado ao banco de dados com sucesso!")
+} catch (error) {
+    console.log(error);
+}
+
+export { documentosColecao };
