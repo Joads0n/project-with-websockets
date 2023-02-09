8eea3c823893225d5e88b85bf636f9e0214f3526
Joadson Silva <joadson83@hotmail.com>
2023-02-08 23:39:46 -0300

    Cadastro no banco de dados

--- public/cadastro/socket-front-cadastro.js
+++ public/cadastro/socket-front-cadastro.js
@@ -4,4 +4,12 @@ function emitirCadastrarUsuario(data) {
     socket.emit("cadastrar_usuario", data);
 }
 
+socket.on("cadastro_sucesso", () => {
+    alert("Cadastro realizado com sucesso");
+});
+
+socket.on("cadastro_erro", () => {
+    alert("Erro no cadastro");
+});
+
 export { emitirCadastrarUsuario };
--- server/recordEvents/register.js
+++ server/recordEvents/register.js
@@ -3,6 +3,12 @@ import { registryUser } from "../database/usersDB.js";
 function recordEventsRegister(socket, io) {
     socket.on("cadastrar_usuario", async (data) => {
         const result = await registryUser(data);
+
+        if (result.acknowledged) {
+            socket.emit("cadastro_sucesso");
+        } else {
+            socket.emit("cadastro_erro");
+        }
     });
 }
 
